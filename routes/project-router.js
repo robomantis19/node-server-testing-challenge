20c370044f30e899a801867022dd431a80626ee0
robomantis19 <freefavors@gmail.com>
2020-01-31 07:55:59 -0500

    happy coding errors

--- routes/project-router.js
+++ routes/project-router.js
@@ -10,11 +10,15 @@ router.get('/churchs', (req, res) => {
     .then(churchs => { 
         res.status(200).json({churchs});
     })
+    .catch(err => { 
+        console.log('get request error': err)
+        res.status(500).json({message: "error in get request: ", error: err})
+    })
 })
 router.post('/set', (req, res) => { 
     console.log('router req', req.body)
     Input.add(req.body)
-    
+
     .then(input => { 
         console.log('router churchs', input)
         res.status(201).json(input);
